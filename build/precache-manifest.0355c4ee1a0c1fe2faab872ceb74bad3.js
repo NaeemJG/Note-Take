019b6326d3e367359cd33e799c0b4341d24c706b
Jamil Naeem <jamil.naeem17@gmail.com>
2020-07-02 22:51:06 -0400

    Ran npm run build to prepare to deploy on heroku

---
+++ build/precache-manifest.0355c4ee1a0c1fe2faab872ceb74bad3.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9abcf8799c7e02b52cd60e47b047ea28",
+    "url": "/index.html"
+  },
+  {
+    "revision": "718595d64cd99baabb02",
+    "url": "/static/js/2.0b29e6f4.chunk.js"
+  },
+  {
+    "revision": "a033d415ff8986e80981",
+    "url": "/static/js/main.ddd7936f.chunk.js"
+  },
+  {
+    "revision": "4bd54ba1b6cf294e65b1",
+    "url": "/static/js/runtime-main.9e8ab56b.js"
+  }
+]);
